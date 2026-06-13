ec38f0a5178dd5658b103f68272d4ac97517a85c
Eren Sarialp <erensrlp@gmail.com>
2026-06-13 22:57:30 +0300

    Update

---
+++ NetCoreAI.Project2_ApiConsumeUI/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreAI.Project2_ApiConsumeUI.Dtos
+{
+    public class CreateCustomerDto
+    {
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public decimal CustomerBalance { get; set; }
+    }
+}
