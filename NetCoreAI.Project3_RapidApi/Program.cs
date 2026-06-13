7990b2319bedf993c6abd07e13862214bb9aa0f2
Eren Sarialp <erensrlp@gmail.com>
2026-06-13 23:08:48 +0300

    Add Newtonsoft.Json and implement API data retrieval
    Updated `NetCoreAI.Project3_RapidApi.csproj` to include
    `Newtonsoft.Json` version `13.0.4` for JSON handling.
    Refactored `Program.cs` to remove default console code and
    added functionality to fetch and display IMDb's top 100
    movies series using an `HttpClient`.

--- NetCoreAI.Project3_RapidApi/Program.cs
+++ NetCoreAI.Project3_RapidApi/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+using NetCoreAI.Project3_RapidApi.ViewModels;
+using Newtonsoft.Json;
+
+
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "918175620amshb41fd10e67b1fcap1e3449jsnbd3361c5cf89" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach(var series in apiSeriesViewModels)
+    {
+        Console.WriteLine(series.rank + "-" + series.title + "-Film Puanı: " + series.rating + "-Yapım Yılı: " + series.year);
+    }
+    
+}
+
 Console.ReadLine();
