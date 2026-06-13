806b76035e3e9abab3319dd7ae71915bc187953f
Eren Sarialp <erensrlp@gmail.com>
2026-06-13 21:05:44 +0300

    Add project files.

---
+++ NetCoreAI.Project1_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project1_ApiDemo.Entities;
+
+namespace NetCoreAI.Project1_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-J4RS4FL; initial catalog = ApiAIDb; integrated security = true; trustservercertificate = true");
+        }
+
+        public DbSet<Customer> Customers { get; set; } 
+    }
+}
