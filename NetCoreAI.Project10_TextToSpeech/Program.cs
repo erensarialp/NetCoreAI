7d35e6c365d816ee220a02d9ac728b9fbc59eb6e
Eren Sarialp <erensrlp@gmail.com>
2026-06-18 21:33:54 +0300

    Add TextToSpeech project with speech synthesis feature
    This commit introduces a new project named "NetCoreAI.Project10_TextToSpeech" to the solution. The project is configured to use the .NET SDK and targets .NET 9.0, including a reference to the `System.Speech` package.
    
    Additionally, a `Program.cs` file has been created, which implements a console application that prompts the user for input and reads the text aloud using the `SpeechSynthesizer` class.

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+        speechSynthesizer.Volume = 100;
+        speechSynthesizer.Rate = 0;
+
+        Console.Write("Metni Girin: ");
+        string input;
+        input = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(input))
+        {
+            speechSynthesizer.Speak(input);
+        }
+
+        Console.ReadLine();
+    }
+}
